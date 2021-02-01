9f45456fd0116c5001ccb6a28fb8e7843b588df5
PP-62 <77794838+PP-62@users.noreply.github.com>
2021-02-01 20:30:45 +0300

    Превью основного окна
    окно, кнопки, текста.
    добавить Pillow.

---
+++ Gui.py
@@ -0,0 +1,63 @@
+import tkinter as tk
+# надо импортировать Pillow
+
+main_set = {"geometry":"500x500",
+        "background":"white",
+        "title":"meme game"}
+
+meme = {"name":"имя",
+        "text":"подпись",
+    "info":"Инфа о меме",
+        "img":"image_link"}
+
+
+canv_set = {"height":300,
+        "width":400,
+        "background":"white"}
+
+name_meme_set = {"color":"black",
+                 "font":"Arial 14",
+                 "height":2,
+                 "width":20}
+
+text_meme_set = {"color":"black",
+                 "font":"Arial 8",
+                 "height":2,
+                 "width":20}
+
+button_set = {"height":1,
+              "width":6,
+              "color":"black",
+              "font":"Arial 14",
+              "like":"like",
+              "like_color":"green",
+              "dislike":"dislike",
+              "dislike_color":"red"}
+
+#root
+root = tk.Tk()
+root.title(main_set["title"])
+root.geometry(main_set["geometry"])
+root.resizable(False, False)
+
+#elements_place
+meme_image = ""
+#meme_image = tk.PhotoImage(file = meme["img"])
+#добавить изображение в канвас
+
+meme_name = tk.Label(root,text = meme["name"],fg = name_meme_set["color"],font = name_meme_set["font"],height = name_meme_set["height"],width = name_meme_set["width"])
+meme_text = tk.Label(root,text = meme["text"],fg = text_meme_set["color"],font = text_meme_set["font"],height = text_meme_set["height"],width = text_meme_set["width"])
+canvas = tk.Canvas(root,height =canv_set["height"],width = canv_set["width"],bg = canv_set["background"])
+buttons=tk.Frame(root,height = button_set["height"],width = button_set["width"]*2)
+like = tk.Button(buttons,height = button_set["height"],width = button_set["width"],text = button_set["like"],bg = button_set["like_color"],font = button_set["font"])
+dislike = tk.Button(buttons,height = button_set["height"],width = button_set["width"],text = button_set["dislike"],bg = button_set["dislike_color"],font = button_set["font"])
+
+#pack
+meme_name.pack(side='top')
+canvas.pack(side='top')
+meme_text.pack(side='top')
+buttons.pack(side='top')
+
+like.grid(row = 1,column = 1)
+dislike.grid(row = 1,column = 2)
+
