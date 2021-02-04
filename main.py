78632cec42b11cc66436585c3c3001db55a7f5cb
GavrWampus <46278692+GavrWampus@users.noreply.github.com>
2021-02-04 19:56:09 +0300

    +Виртуальная среда
    Добавлена виртуальная среда(см. README). Чтобы сделать окно(сцену) "game", было бы круто заиметь папочку с библиотекой мемов.

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
+''' IMPORTS '''
 from tkinter import *
 from PIL import Image, ImageTk
 
+''' VARIABLES '''
 # Окно создаёться...
 menu = Tk()
 menu.title('Мемы определяют возраст')
 menu.geometry('500x400+300+200')
 
 # Загрузка изображений
-like = Image.open('like.jpg')
-like = like.resize((50, 50), Image.ANTIALIAS)
+like = Image.open('Buttons/like.jpg')
+like = like.resize((60, 55), Image.ANTIALIAS)
 photoLike = ImageTk.PhotoImage(like)
 
-
+''' FUNCTIONS '''
 def mQuit():
     menu.quit()
 
@@ -24,6 +26,7 @@ def mInfo():
 def mStart():
     pass
 
+''' BUTTONS '''
 #quit
 quitButton = Button(menu, bg='gray', fg='white', text='Выход', command=mQuit)
 quitButton.place(relx=0.4,  rely=0.8, relwidth=0.2,  relheight=0.1)
@@ -38,7 +41,7 @@ startButton.place(relx=0.4,  rely=0.65, relwidth=0.2,  relheight=0.1)
 
 #like
 startButton = Button(menu, image=photoLike )
-startButton.place(relx=0.4,  rely=0.50, relwidth=0.1,  relheight=0.1)
+startButton.place(relx=0.4,  rely=0.50, relwidth=0.11,  relheight=0.13)
 
 
 menu.mainloop() # Главный обработчик событий
