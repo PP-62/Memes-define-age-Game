303e71451f85993505fd3d463b70c7fcfd219736
PP-62 <77794838+PP-62@users.noreply.github.com>
2021-06-15 23:13:16 +0300

    first +-work version 1

--- venv/Scripts/Activate.ps1
+++ venv/Scripts/Activate.ps1
@@ -27,7 +27,7 @@ function global:deactivate ([switch]$NonDestructive) {
 
 deactivate -nondestructive
 
-$env:VIRTUAL_ENV="C:\Users\Пользователь\Documents\GitHub\Memes-define-age-Game\venv"
+$env:VIRTUAL_ENV="C:\meme_define_game\venv"
 
 if (! $env:VIRTUAL_ENV_DISABLE_PROMPT) {
     # Set the prompt to include the env name
